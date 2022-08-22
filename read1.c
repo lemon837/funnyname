db2c4e2570ec9c225b49b98a58be110f121ca6b1
lemon837 <105208298+lemon837@users.noreply.github.com>
2022-08-22 12:42:55 +0800

    Add files via upload

---
+++ read1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define MAX_LINES 100
+#define MAX_LEN 1000
+
+int main (void)
+{
+	char data[MAX_LINES][MAX_LEN];
+	FILE* file;
+
+	file = fopen("crontab-file.txt", "r");
+	if (file == NULL) {
+		printf("Error opening file");
+		return 1;
+	}
+
+	int line = 0;
+
+	while (!feof(file) && !ferror(file)) {
+		if (fgets(data[line], MAX_LEN, file) != NULL) {
+			line++;
+		}
+	}
+
+	fclose(file);
+	
+	for (int i = 0; i < line; i++ ) {
+   		printf("%s", data[i]);
+	}
+
+	return 0;
+}
