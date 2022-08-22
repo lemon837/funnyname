f1e2aeadd46f4cbd4ffc6c37ab0f2ac72e451a29
ausj3710 <111719728+ausj3710@users.noreply.github.com>
2022-08-22 13:14:10 +0800

    Add files via upload

--- estimatecron.c
+++ estimatecron.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <ctype.h>
@@ -10,31 +9,48 @@
 #include <inttypes.h>
 #include <sys/time.h>
 
-void readfile1 (char filename[]) 
-{
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
-      perror("Unable to open file");
-      exit(EXIT_FAILURE);
-    }
+#include <stdio.h>
 
-    char *line = NULL;
-    size_t len = 0;
+#define MAX_LINES 100
+#define MAX_LEN 1000
 
-    while (getline(&line, &len, fp) != -1) {
-      printf("%s", line);
-    }
+void readfile1 (char filename[]) {
+	char data[MAX_LINES][MAX_LEN];
+	FILE* file;
 
-    fclose(fp);
-    free(line);
+	file = fopen("crontab-file.txt", "r");
+	if (file == NULL) {
+		printf("Error opening file");
+		exit (EXIT_FAILURE);
+	}
+
+	int line = 0;
+
+	while (!feof(file) && !ferror(file)) {
+		if (fgets(data[line], MAX_LEN, file) != NULL) {
+			line++;
+		}
+	}
+
+	fclose(file);
+
+	for (int i = 0; i < line; i++) {
+		printf("%s", data[i]);
+	}
 }
+
+
 
 int main (int argc, char* argv[])
 {
         if (argc !=4) {
-                fprintf(stderr, "Usage: ./estimatecron month crontab-file estimates-file");
+                fprintf(stderr, "Usage: ./estimatecron month crontab-file estimates-file\n");
                 exit(EXIT_FAILURE);
         }
+	if ((strcmp (argv[2], "jan")) == 0) {
+		printf ("I'm Jan");
+	}
+			
         else {
                 readfile1(argv[3]);
                 readfile1(argv[4]);
